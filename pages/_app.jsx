ade772b83ddf901e63fc57e2cbb61651e715f90d
iamsahebgiri <iamsahebgiri@gmail.com>
2020-09-01 00:08:39 +0530

    ADDED :rocket: : NProgress and Configured with the app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,19 @@
+import dynamic from 'next/dynamic';
 import '../styles/index.css';
+import '../styles/nprogress.css';
+
+const TopProgressBar = dynamic(
+  () => import('./../components/topProgressBar/index'),
+  { ssr: false },
+);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <TopProgressBar />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
