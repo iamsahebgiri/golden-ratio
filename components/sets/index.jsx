0c5552e5c0d70e02aef63490fbf266f8de73c5ea
iamsahebgiri <iamsahebgiri@gmail.com>
2020-09-04 09:07:20 +0530

    Refactored!

--- components/sets/index.jsx
+++ components/sets/index.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
+function SetBox({ name, img }) {
+  return (
+    <div className="mr-5">
+      <img className="h-32 w-32 bg-gray-300 rounded-lg" src={`./sets/${img}`} alt={name} />
+      <p className="h-5 w-32 mt-3 text-center">{name}</p>
+    </div>
+  );
+}
+
 export default function index() {
   return (
     <div className="container mx-auto mb-12 md:mb-16 md:p-8">
@@ -9,40 +18,14 @@ export default function index() {
         Sets
       </div>
       <div className="flex px-4 mt-8 overflow-x-scroll hide-scroll">
-        {/* <div className="h-32 w-32 bg-gray-300 rounded-lg" /> */}
-        {/* <div className="h-5 w-32 bg-gray-300 rounded-full mt-3" /> */}
-        <div className="mr-5">
-          <img className="h-32 w-32 bg-gray-300 rounded-lg" src="./sets/square.png" alt="Square" />
-          <p className="h-5 w-32 mt-3 text-center">Suqare</p>
-        </div>
-        <div className="mr-5">
-          <img className="h-32 w-32 bg-gray-300 rounded-lg" src="./sets/5_4.png" alt="5:4" />
-          <p className="h-5 w-32 mt-3 text-center">5:4</p>
-        </div>
-        <div className="mr-5">
-          <img className="h-32 w-32 bg-gray-300 rounded-lg" src="./sets/4_3.png" alt="4:3" />
-          <p className="h-5 w-32 mt-3 text-center">4:3</p>
-        </div>
-        <div className="mr-5">
-          <img className="h-32 w-32 bg-gray-300 rounded-lg" src="./sets/silver_ratio.png" alt="Silver Ratio" />
-          <p className="h-5 w-32 mt-3 text-center">Silver Ratio</p>
-        </div>
-        <div className="mr-5">
-          <img className="h-32 w-32 bg-gray-300 rounded-lg" src="./sets/golden_ratio.png" alt="Golden Ratio" />
-          <p className="h-5 w-32 mt-3 text-center">Golden Ratio</p>
-        </div>
-        <div className="mr-5">
-          <img className="h-32 w-32 bg-gray-300 rounded-lg" src="./sets/platinum_ratio.png" alt="Platinum Ratio" />
-          <p className="h-5 w-32 mt-3 text-center">Platinum Ratio</p>
-        </div>
-        <div className="mr-5">
-          <img className="h-32 w-32 bg-gray-300 rounded-lg" src="./sets/16_9.png" alt="16:9" />
-          <p className="h-5 w-32 mt-3 text-center">16:9</p>
-        </div>
-        <div className="mr-5">
-          <img className="h-32 w-32 bg-gray-300 rounded-lg" src="./sets/bronze_ratio.png" alt="Bronze Ratio" />
-          <p className="h-5 w-32 mt-3 text-center">Bronze Ratio</p>
-        </div>
+        <SetBox name="Square" img="square.png" />
+        <SetBox name="5:4" img="5_4.png" />
+        <SetBox name="4:3" img="4_3.png" />
+        <SetBox name="Silver Ratio" img="silver_ratio.png" />
+        <SetBox name="Golden Ratio" img="golden_ratio.png" />
+        <SetBox name="Platinum Ratio" img="platinum_ratio.png" />
+        <SetBox name="16:9" img="16_9.png" />
+        <SetBox name="Bronze Ratio" img="bronze_ratio.png" />
       </div>
     </div>
   );
