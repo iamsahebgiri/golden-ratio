5fb4e1109bac653f15f06d83dc93265fdc34320e
iamsahebgiri <iamsahebgiri@gmail.com>
2020-09-07 12:18:42 +0530

    Update index.jsx

--- components/facts/index.jsx
+++ components/facts/index.jsx
@@ -2,13 +2,13 @@ import { RiQuestionFill } from 'react-icons/ri';
 
 export default function Facts() {
   return (
-    <div className="container mx-auto mb-12 md:mb-16 md:p-8">
-      <div className="font-heading leading-tight text-3xl md:text-4xl font-semibold capitalize px-4 text-neutral">
+    <div className="container mx-auto mb-12 md:mb-16 md:px-8">
+      <div className="font-heading leading-tight text-3xl md:text-4xl font-semibold capitalize px-4 md:p-0  text-neutral">
         Interesting
         <span className="text-highlight"> Facts </span>
         about Phi
       </div>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 px-4 mt-8 overflow-x-scroll hide-scroll ">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 px-4 md:p-0 mt-8">
         <div className="p-5 pb-8 bg-red-brand rounded-lg">
           <div className="flex flex-row-reverse">
             <RiQuestionFill color="#FFF" size="32" />
