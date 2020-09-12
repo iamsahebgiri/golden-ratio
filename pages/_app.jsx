33a0a68f0631fe896ea810ddfe2fd54101652c23
iamsahebgiri <iamsahebgiri@gmail.com>
2020-09-12 09:48:36 +0530

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,5 @@
 import dynamic from 'next/dynamic';
+import Head from 'next/head';
 import '../styles/index.css';
 import '../styles/nprogress.css';
 
@@ -10,6 +11,13 @@ const TopProgressBar = dynamic(
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Golden Ratio - The Divine Proportion - HackBenchers 4 </title>
+        <link rel="shortcut icon" type="image/jpg" href="./icons/edit_img.png " />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content="Golden Ratio - The Divine Proportion - HackBenchers 4" key="title" />
+      </Head>
+
       <TopProgressBar />
       <Component {...pageProps} />
     </>
